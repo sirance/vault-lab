e61678704f6c088660493eb834dd85ec4426a4f2
Simon Rance <sirance@gmail.com>
2023-05-24 15:05:10 +0100

    uncommented code

--- policies.tf
+++ policies.tf
@@ -1,5 +1,5 @@
 
 resource "vault_policy" "kv_all" {
-  #   name   = "kv_all"
-  #   policy = file("${path.module}/hcl/kv_all.hcl")
+  name   = "kv_all"
+  policy = file("${path.module}/hcl/kv_all.hcl")
 }
