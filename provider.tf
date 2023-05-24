c137e25bc3b71fab8ce8717da835c0632f034b1f
Simon Rance <sirance@gmail.com>
2023-05-24 15:20:04 +0100

    skip tls.... again

--- provider.tf
+++ provider.tf
@@ -1,6 +1,7 @@
 provider "vault" {
-  address = var.vault_url
-  token   = var.vault_token
+  address         = var.vault_url
+  token           = var.vault_token
+  skip_tls_verify = true
   # auth_login_jwt {
   #   mount = "jwt"
   #   role  = "github-actions-role"
