a755952c937f63b471f507cab21a2a9eaba67522
Simon Rance <sirance@gmail.com>
2023-05-25 14:25:17 +0100

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -2,8 +2,8 @@ provider "vault" {
   # address         = var.vault_url
   # token           = var.vault_token
   skip_tls_verify = true
-  # auth_login_jwt {
-  #   mount = "jwt"
-  #   role  = "github-actions-role"
-  # }
+  auth_login_jwt {
+    mount = "jwt"
+    role  = "github-actions-role"
+  }
 }
